233497a4ffdae3502f647076754c56cd95f69b85
Martin.Johannes.Nilsen <martin.johannes.nilsen@ciber.no>
2020-10-05 12:11:43 +0200

    Updates

---
+++ precache-manifest.b562a81fe096fd8bcc59a91e059e5934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f12d5da9ffa8ff9e1384622b07c5da",
+    "url": "/dagpengerkalkulatorNAV/index.html"
+  },
+  {
+    "revision": "1bf87904e1fd2f138467",
+    "url": "/dagpengerkalkulatorNAV/static/css/main.c992cd0e.chunk.css"
+  },
+  {
+    "revision": "a9945af9ec1a4f447315",
+    "url": "/dagpengerkalkulatorNAV/static/js/2.9423a1bb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dagpengerkalkulatorNAV/static/js/2.9423a1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf87904e1fd2f138467",
+    "url": "/dagpengerkalkulatorNAV/static/js/main.d30d66e3.chunk.js"
+  },
+  {
+    "revision": "9dbbeddc362573f74b80",
+    "url": "/dagpengerkalkulatorNAV/static/js/runtime-main.2b7d746e.js"
+  }
+]);
